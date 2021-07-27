66d08f4a3cd72b9afbcc27f82e26d2cf461aa164
nhuse <nathan.huse@yahoo.com>
2021-07-27 15:11:24 -0500

    commit tree 91525751dd2664f08fa8f678733cc93b909904c4

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState({type: "all"});
+
+  function onChangeType(type) {
+    console.log(type)
+    const newObj = {type: type}
+    setFilters(newObj)
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pets`)
+    .then(resp => resp.json())
+    .then(json => setPets(json))
+  }, [])
+
+  if(pets.length===0) {
+    return (<h1>Loading...</h1>)
+  }
+
+  function onFindPetsClick() {
+    if(filters.type === "all") {
+      fetch(`http://localhost:3001/pets`)
+      .then(resp => resp.json())
+      .then(json => setPets(json))
+    }else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(resp => resp.json())
+      .then(json => setPets(json))
+    }
+  }
+
+  function onAdoptPet(id) {
+    pets.map((pet) => {
+      if(pet.id === id){
+        pet.isAdopted = true;
+      }})
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +50,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
